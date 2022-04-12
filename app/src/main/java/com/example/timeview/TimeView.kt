1abbc4165435f63a526c9afc4f2d305b2c98ce2d
MikhailKrautsevich <mihail4egevara@gmail.com>
2022-04-12 21:40:41 +0300

    Code Style (m- & s-)

--- app/src/main/java/com/example/timeview/TimeView.kt
+++ app/src/main/java/com/example/timeview/TimeView.kt
@@ -17,52 +17,52 @@ class TimeView @JvmOverloads constructor(
         private const val LOG = "TimeView.LOG"
     }
 
-    private var mCenterX = 0F
-    private var mCenterY = 0F
+    private var centerX = 0F
+    private var centerY = 0F
 
     // длина стрелок относительно радиуса циферблата
-    private var mSecHandLenPercents = 0.85F
-    private var mMinHandLenPercents = 0.8F
-    private var mHourHandLenPercents = 0.5F
+    private var secHandLenPercents = 0.85F
+    private var minHandLenPercents = 0.8F
+    private var hourHandLenPercents = 0.5F
 
-    private var mClockFaceRadius = 0F
-    private var mClockLabelLenght = 0F
-    private var mSecHandRadius = 0F
-    private var mMinHandRadius = 0F
-    private var mHourHandRadius = 0F
+    private var clockFaceRadius = 0F
+    private var clockLabelLenght = 0F
+    private var secHandRadius = 0F
+    private var minHandRadius = 0F
+    private var hourHandRadius = 0F
     // длина задней части стрелки
-    private var mClockHandBackLength = 0F
+    private var clockHandBackLength = 0F
 
     // толщина линии циферблата и минутной стрелки
-    private var mDefPaintThickness = 15F
-    private var mSecHandThickness = 10F
-    private var mHourHandThickness = 25F
+    private var defPaintThickness = 15F
+    private var secHandThickness = 10F
+    private var hourHandThickness = 25F
 
-    private var mClockFaceColor = Color.CYAN
-    private var mSecHandColor = Color.CYAN
-    private var mMinHandColor = Color.CYAN
-    private var mHourHandColor = Color.CYAN
+    private var clockFaceColor = Color.CYAN
+    private var secHandColor = Color.CYAN
+    private var minHandColor = Color.CYAN
+    private var hourHandColor = Color.CYAN
 
-    private val mPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        strokeWidth = mDefPaintThickness
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        strokeWidth = defPaintThickness
         style = Paint.Style.STROKE
-        color = mClockFaceColor
+        color = clockFaceColor
     }
 
-    private var mTime: Calendar = GregorianCalendar.getInstance()
+    private var calendar: Calendar = GregorianCalendar.getInstance()
 
     inner class TimeViewUpdateTask : TimerTask() {
         override fun run() {
-            mTime = GregorianCalendar.getInstance()
+            calendar = GregorianCalendar.getInstance()
             invalidate()
         }
     }
 
-    private val sTimer = Timer()
-    private val sTask = TimeViewUpdateTask()
+    private val timer = Timer()
+    private val task = TimeViewUpdateTask()
 
     init {
-        sTimer.schedule(sTask, 50, 50)
+        timer.schedule(task, 50, 50)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -70,16 +70,17 @@ class TimeView @JvmOverloads constructor(
 
         Log.d(LOG, "onMeasure")
 
-        mCenterX = measuredWidth / 2F
-        mCenterY = measuredHeight / 2F
-        mClockFaceRadius = if (measuredWidth > measuredHeight) {
-            mCenterY * 0.95F
-        } else mCenterX * 0.95F
-        mClockLabelLenght = mClockFaceRadius * 0.1F
-        mSecHandRadius = mClockFaceRadius * mSecHandLenPercents
-        mMinHandRadius = mClockFaceRadius * mMinHandLenPercents
-        mHourHandRadius = mClockFaceRadius * mHourHandLenPercents
-        mClockHandBackLength = mHourHandRadius * 0.5F
+        centerX = measuredWidth / 2F
+        centerY = measuredHeight / 2F
+        val minSide  = if (measuredWidth > measuredHeight) {
+            measuredHeight
+        } else measuredWidth
+        clockFaceRadius = 0.5F * minSide * 0.95F
+        clockLabelLenght = clockFaceRadius * 0.1F
+        secHandRadius = clockFaceRadius * secHandLenPercents
+        minHandRadius = clockFaceRadius * minHandLenPercents
+        hourHandRadius = clockFaceRadius * hourHandLenPercents
+        clockHandBackLength = hourHandRadius * 0.5F
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -89,33 +90,33 @@ class TimeView @JvmOverloads constructor(
 
         canvas?.let {
             drawClockFace(canvas)
-            drawSecHand(canvas, mTime)
-            drawMinHand(canvas, mTime)
-            drawHourHand(canvas, mTime)
+            drawSecHand(canvas, calendar)
+            drawMinHand(canvas, calendar)
+            drawHourHand(canvas, calendar)
         }
     }
 
     private fun drawClockFace(canvas: Canvas) {
         canvas.save()
-        mPaint.apply {
-            strokeWidth = mDefPaintThickness
-            color = mClockFaceColor
+        paint.apply {
+            strokeWidth = defPaintThickness
+            color = clockFaceColor
         }
-        canvas.drawCircle(mCenterX, mCenterY, mClockFaceRadius, mPaint)
+        canvas.drawCircle(centerX, centerY, clockFaceRadius, paint)
         for (i in 0..11) {
             drawClockLabel(canvas)
-            canvas.rotate(30F, mCenterX, mCenterY)
+            canvas.rotate(30F, centerX, centerY)
         }
         canvas.restore()
     }
 
     private fun drawClockLabel(canvas: Canvas) {
         canvas.drawLine(
-            mCenterX,
-            mCenterY - mClockFaceRadius,
-            mCenterX,
-            mCenterY - mClockFaceRadius + mClockLabelLenght,
-            mPaint
+            centerX,
+            centerY - clockFaceRadius,
+            centerX,
+            centerY - clockFaceRadius + clockLabelLenght,
+            paint
         )
     }
 
@@ -124,18 +125,18 @@ class TimeView @JvmOverloads constructor(
         val millies = time.get(Calendar.MILLISECOND)
         val angle = getSecHandRotateAngle(secs, millies)
         Log.d(LOG, "drawSecHand: secs = $secs, millies = $millies, angle = $angle")
-        mPaint.apply {
-            strokeWidth = mSecHandThickness
-            color = mSecHandColor
+        paint.apply {
+            strokeWidth = secHandThickness
+            color = secHandColor
         }
         canvas.save()
-        canvas.rotate(angle, mCenterX, mCenterY)
+        canvas.rotate(angle, centerX, centerY)
         canvas.drawLine(
-            mCenterX,
-            mCenterY - mSecHandRadius,
-            mCenterX,
-            mCenterY + mClockHandBackLength,
-            mPaint
+            centerX,
+            centerY - secHandRadius,
+            centerX,
+            centerY + clockHandBackLength,
+            paint
         )
         canvas.restore()
     }
@@ -145,18 +146,18 @@ class TimeView @JvmOverloads constructor(
         val secs = time.get(Calendar.SECOND)
         val angle = getMinHandRotateAngle(mins, secs)
         Log.d(LOG, "drawSecHand: mins = $mins, secs = $secs, angle = $angle")
-        mPaint.apply {
-            strokeWidth = mDefPaintThickness
-            color = mMinHandColor
+        paint.apply {
+            strokeWidth = defPaintThickness
+            color = minHandColor
         }
         canvas.save()
-        canvas.rotate(angle, mCenterX, mCenterY)
+        canvas.rotate(angle, centerX, centerY)
         canvas.drawLine(
-            mCenterX,
-            mCenterY - mMinHandRadius,
-            mCenterX,
-            mCenterY + mClockHandBackLength,
-            mPaint
+            centerX,
+            centerY - minHandRadius,
+            centerX,
+            centerY + clockHandBackLength,
+            paint
         )
         canvas.restore()
     }
@@ -166,18 +167,18 @@ class TimeView @JvmOverloads constructor(
         val mins = time.get(Calendar.MINUTE)
         val angle = getHourHandRotateAngle(hours, mins)
         Log.d(LOG, "drawSecHand: hours = $hours, mins = $mins, angle = $angle")
-        mPaint.apply {
-            strokeWidth = mHourHandThickness
-            color = mHourHandColor
+        paint.apply {
+            strokeWidth = hourHandThickness
+            color = hourHandColor
         }
         canvas.save()
-        canvas.rotate(angle, mCenterX, mCenterY)
+        canvas.rotate(angle, centerX, centerY)
         canvas.drawLine(
-            mCenterX,
-            mCenterY - mHourHandRadius,
-            mCenterX,
-            mCenterY + mClockHandBackLength,
-            mPaint
+            centerX,
+            centerY - hourHandRadius,
+            centerX,
+            centerY + clockHandBackLength,
+            paint
         )
         canvas.restore()
     }
